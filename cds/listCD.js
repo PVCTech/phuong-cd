a8abb8ac85b7ef7e4cdf60a570717474f7347a11
phamcuong8868 <phamcuong8868@gmail.com>
2025-09-26 10:12:08 +0700

    editing

--- cds/listCD.js
+++ cds/listCD.js
@@ -3,58 +3,34 @@ function cloneBaiHat(bh) {
 }
 
 var allCD = [];
-var cdChaoCo = [];
 
-cdChaoCo.push(cloneBaiHat(listNhacMaiLinh[0]));
-cdChaoCo.push(cloneBaiHat(listNhacLe[0]));
-cdChaoCo.push(cloneBaiHat(listNhacMaiLinh[1]));
-
-for (let i = 2; i < listNhacMaiLinh.length; i++) {
-    cdChaoCo.push(cloneBaiHat(listNhacMaiLinh[i]));
+///////////////////////////////////////////////
+var cdHangNgay = [];
+for (let i = 2; i < listTruongTranBichSan.length; i++) 
+{
+    cdHangNgay.push(cloneBaiHat(listTruongTranBichSan[i]));
 }
 
-listNhacNen.forEach(e => {
+for (let i = 0; i < cdHangNgay.length; i++) 
+{
+    cdHangNgay[i].trackId = i;
+    cdHangNgay[i].mediaId = `music_${i}`;
+}
+allCD.push(cdHangNgay);
+
+
+
+
+////////////////////////////////////////////////
+var cdChaoCo = [];
+listNhacLe.forEach(e =>
+{
     cdChaoCo.push(cloneBaiHat(e));
 });
 
-listQueHuongCachMang.forEach(e => {
-    cdChaoCo.push(cloneBaiHat(e));
-});
-
-for (let i = 0; i < cdChaoCo.length; i++) {
+for (let i = 0; i < cdChaoCo.length; i++) 
+{
     cdChaoCo[i].trackId = i;
     cdChaoCo[i].mediaId = `music_${i}`;
 }
-
 allCD.push(cdChaoCo);
-
-var cdNhacDangCongSan = [];
-listNhacLe.forEach(e =>
-    {
-        cdNhacDangCongSan.push(cloneBaiHat(e));
-    }
-);
-
-listDangCongSan.forEach(e =>
-    {
-        cdNhacDangCongSan.push(cloneBaiHat(e));
-    }
-);
-for (let i = 0; i < cdNhacDangCongSan.length; i++) {
-    cdNhacDangCongSan[i].trackId = i;
-    cdNhacDangCongSan[i].mediaId = `music_${i}`;
-}
-
-allCD.push(cdNhacDangCongSan);
-
-var cdSinhNhat = [];
-listSinhNhat.forEach(e =>
-{
-    cdSinhNhat.push(cloneBaiHat(e));
-});
-
-for (let i = 0; i < cdSinhNhat.length; i++) {
-    cdSinhNhat[i].trackId = i;
-    cdSinhNhat[i].mediaId = `music_${i}`;
-}
-allCD.push(cdSinhNhat);
